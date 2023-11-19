9da88b8d7b1cfdb654a1ce46d1b2e0d3c1b7eb1b
Fatima Ezzahra Houd <f.houd@etu.enset-media.ac.ma>
2023-11-19 18:00:35 +0100

    init prj

---
+++ src/test/java/com/houd/comptecqrs/CompteCqrsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.houd.comptecqrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompteCqrsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
